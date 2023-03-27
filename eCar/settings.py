49e97652041db3331f79aa6501b1d083bc4b6957
UtsavK27 <urkanani27@gmail.com>
2023-03-27 19:07:05 +0530

    thirs commit

--- eCar/settings.py
+++ eCar/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'eCar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eCar_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
